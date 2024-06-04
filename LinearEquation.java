259fb026cc26503ed7890630aec8a8b84d99cc32
Muhammad Riza Abqari <143402936+MrAbqari@users.noreply.github.com>
2024-06-04 11:14:54 +0700

    LinearEquation.java

--- LinearEquation.java
+++ LinearEquation.java
@@ -1,58 +1,30 @@
 package tugas.pkg7.pbo;
 
-public class LinearEquation {
+public class StopWatch {
+	private long startTime;
+	private long endTime;
 
-    private double a;
-    private double b;
-    private double c;
-    private double d;
-    private double e;
-    private double f;
+	public StopWatch() {
+		startTime = System.currentTimeMillis();
+	}
 
-    public LinearEquation(double a, double b, double c, double d, double e, double f) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        this.e = e;
-        this.f = f;
-    }
+	public void start() {
+		startTime = System.currentTimeMillis();
+	} 
 
-    public double getA() {
-        return a;
-    }
+	public void stop() {
+		endTime = System.currentTimeMillis();
+	}
 
-    public double getB() {
-        return b;
-    }
+	public long getElapsedTime() {
+		return getEndTime() - getStartTime();
+	}
 
-    public double getC() {
-        return c;
-    }
+	public long getStartTime() {
+		return startTime;
+	}
 
-    public double getD() {
-        return d;
-    }
-
-    public double getE() {
-        return e;
-    }
-
-    public double getF() {
-        return f;
-    }
-
-    // Method to check if the system is solvable
-    public boolean isSolvable() {
-        return a * d - b * c != 0;
-    }
-
-    // Methods to get x and y solutions
-    public double getX() {
-        return (e * d - b * f) / (a * d - b * c);
-    }
-
-    public double getY() {
-        return (a * f - e * c) / (a * d - b * c);
-    }
+	public long getEndTime() {
+		return endTime;
+	}
 }
